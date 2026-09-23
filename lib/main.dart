cbddf056af643b45eb89eb106a185924476b6cad
Andar24 <andareasdamanik35@gmail.com>
2026-09-23 13:07:13 +0700

    Initial Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'screens/signin_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AnimeVerse',
+      theme: ThemeData(
+        fontFamily: 'Urbanist',
+      ), // ThemeData
+      home: const SignInScreen(),
+      debugShowCheckedModeBanner: false,
+    ); // MaterialApp
+  }
+}
